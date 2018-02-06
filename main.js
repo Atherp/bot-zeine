a180e9237425ff355eda754ada1e3f9383ad3569
Atherp <36198676+Atherp@users.noreply.github.com>
2018-02-06 16:03:25 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+const Discord = require('discord.js');
+
+var bot = new Discord.Client();
+var prefix = ('/');
+
+bot.on('ready', () => {
+    bot.user.setPresence({ game: { name: 'se faire dev par Atherp', type: 0}});
+    console.log('Bot Ready !');
+});
+
+
+bot.login('NDEwMTE2ODk0NjY2OTE1ODQw.DVoepw.sHp1sa5E1H1fYDTghBuly3BPNJs');
+
+bot.on('guildMemberAdd', member => {
+    let role = member.guild.roles.find('name', '► Abos');
+    member.guild.channels.find('name', 'welcome').send(`:hamburger: ${member.user.username} vien de rejoindre la ZEINE ! :fries:`)
+    member.addRole(role)
+
+})
+
+bot.on('guildMemberRemove', member => {
+    member.guild.channels.find('name', 'welcome').send(`:frog: ${member.user.username} vien de quitter la ZEINE, il restera dans nos coeurs à tout jamais ! :frog:`)
+})
+
+
+
+bot.on('message', message => {
+    if (message.content === 'chaussettes'){
+        message.reply('chaussures');
+        console.log('chaussesttes chaussures');
+    }
+
+    if (message.content === prefix + 'help'){
+        var help_embed = new Discord.RichEmbed()
+            .setColor('#FEFEFE')
+            .addField('Commandes du bot !', '   -help : Affiche les commandes du bot !')
+            .addField('Interaction', 'Chaussettes : Le bot répond chaussures !')
+            .setFooter('By α†hεƦρ')
+        message.channel.sendEmbed(help_embed);    
+        //message.channel.send("Voici les commandes du bot :\n -/help pour afficher les commandes");
+        console.log('Commande Help demandée !')
+
+    }
+
+});
